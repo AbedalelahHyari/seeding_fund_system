6564d1820753f9f2faff1c05f0241e9079d082cc
AbedalelahHyari <abedalelah.hyari@gmail.com>
2022-04-05 23:39:40 +0300

    Create new route called usersRoute, it should handle the API requests related to this router

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,13 @@ const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const db = require("./database/db");
-
 app.use(cors());
-
 app.use(express.json());
+/******************************************************** */
+// Import the usersRouter
+const usersRouter = require("./routes/users");
+app.use("/users", usersRouter);
+/********************************************** */
 
 app.listen(process.env.PORT, () => {
   console.log(`SERVER WORKING ON PORT: ${process.env.PORT}`);
