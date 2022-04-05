dbce3415b693863b625edb59bb08ea763d226450
AbedalelahHyari <abedalelah.hyari@gmail.com>
2022-04-05 23:19:37 +0300

    Create the userSchema that will display the user blueprint

---
+++ backend/database/models/users.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const userSchema = new mongoose.Schema({
+  userName: { type: String, required: true },
+  country: { type: String },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: mongoose.Schema.Types.ObjectId, ref: "Role" },
+});
+/******************************************************************************** */
+// This function for hashing the password when the user make a register before saving in the data-base
+userSchema.pre("save", async function () {
+  this.email = this.email.toLowerCase();
+  this.password = await bcrypt.hash(this.password, 10);
+});
+/*************************************************************************** */
+module.exports = mongoose.model("User", userSchema);
