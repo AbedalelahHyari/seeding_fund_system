f8c4898fdc5d3fffbae8edda8b53c6f78c622c91
AbedalelahHyari <abedalelah.hyari@gmail.com>
2022-04-06 15:30:04 +0300

    Create Login Component in the front-end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
-
+import Login from "./components/Login";
+import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <h1>Seeding Funds</h1>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <Login />
     </div>
   );
 }
--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,7 +1,65 @@
-import React from "react";
-
+import axios from "axios";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+/**************************************************************************** */
 const Login = () => {
-  return <div>Login</div>;
-};
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [status, setStatus] = useState("");
+  const navigate = useNavigate();
+  /*************************************** */
+  const login = () => {
+    axios
+      .post("http://localhost:5000/login", {
+        email: email,
+        password: password,
+      })
+      .then((result) => {
+        if (result.data.success === true) {
+          localStorage.setItem("token", result.data.token);
+          console.log(true);
+          //navigate("/dashboard");
+        }
+      })
+      .catch((err) => {
+        setStatus(err.response.data.message);
+      });
+  };
+  return (
+    <>
+      <div className="container w-25">
+        <h1 className="mb-5 mt-5">Login</h1>
+        <div className="form-group">
+          <input
+            required
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            type="email"
+            className="form-control mb-5"
+            placeholder="Email"
+          />
+        </div>
+        <div className="form-group mb-5">
+          <input
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            type="password"
+            placeholder="Password"
+            className="form-control"
+          />
+        </div>
 
+        <button onClick={login} className="btn btn-primary shadow">
+          Login
+        </button>
+        <a href="/register" className="btn btn-secondary m-4 shadow">
+          Go to Register
+        </a>
+        <div class="alert">{status}</div>
+      </div>
+    </>
+  );
+};
 export default Login;
